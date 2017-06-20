387a3fe94d14e96317db2410ef2e5c555a963e7e
driden <driden_87@hotmail.com>
2017-06-20 18:00:44 -0300

    Added styles

--- OpenGameList/Scripts/app/app.component.ts
+++ OpenGameList/Scripts/app/app.component.ts
@@ -2,15 +2,35 @@
 
 @Component({
     selector: "opengamelist",
-    template: `<h1>{{title}}</h1>
-                <div class="menu">
-                    <a class="home" [routerLink]="['']">Home</a>
-                    | <a class="about" [routerLink]="['about']">About</a>
-                    | <a class="login" [routerLink]="['login']">Login</a>
-                    | <a class="add" [routerLink]="['item/edit', 0]">Add New</a>
-                </div>
-                <router-outlet></router-outlet>
-                `
+    template: `
+<nav class="navbar navbar-default navbar-fixed-top">
+	<div class="container-fluid">
+		<input type="checkbox" id="navbar-toggle-cbox"/>
+		<div class="navbar-header">
+			<label for="navbar-toggle-cbox" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</label>
+			<a>
+				<img alt="logo" src="/img/logo.svg"/>
+			</a>
+		</div>
+		<div class="collapse navbar-collapse" id="navbar">
+			<ul class="nav navbar-nav">
+				<li><a class="home" [routerLink]="['']">Home</a></li>
+				<li><a class="about" [routerLink]="['about']">About</a></li>
+				<li><a class="login" [routerLink]="['login']">Login</a></li>
+				<li><a class="add" [routerLink]="['item/edit',0]">Add New</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+<h1 class="header">{{title}}</h1>
+<div class="main-container">
+	<router-outlet></router-outlet>
+</div>`
 })
 
 export class AppComponent{
